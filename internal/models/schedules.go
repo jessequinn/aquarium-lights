23860b003bd890c660dece7389bc461e37493607
Jesse Quinn <me@jessequinn.info>
2021-06-24 13:35:59 -0300

    Update: added comments.

--- internal/models/schedules.go
+++ internal/models/schedules.go
@@ -4,22 +4,26 @@ import (
 	"github.com/stianeikeland/go-rpio"
 )
 
+// SchedulesInterface interface.
 type SchedulesInterface interface {
 	SetModeOutput()
 	SetHigh()
 	GetSchedules() []Schedule
 }
 
+// Schedules struct.
 type Schedules struct {
 	Schedules []Schedule `json:"schedules"`
 }
 
+// Schedule struct.
 type Schedule struct {
 	Name    string   `json:"name"`
 	Pin     rpio.Pin `json:"pin"`
 	Periods []Period `json:"periods"`
 }
 
+// Period struct.
 type Period struct {
 	Start CustomTime `json:"start_time"`
 	End   CustomTime `json:"end_time"`
