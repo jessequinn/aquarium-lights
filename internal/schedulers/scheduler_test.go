716f92a9e950cb48e2dffa8efbc8c836c12fb863
Jesse Quinn <me@jessequinn.info>
2021-06-24 18:09:05 -0300

    Release: 1.0.3
    Signed-off-by: Jesse Quinn <me@jessequinn.info>

--- internal/schedulers/scheduler_test.go
+++ internal/schedulers/scheduler_test.go
@@ -1,17 +1,33 @@
 package schedulers
 
 import (
-	"aquarium-lights/internal/helpers"
-
 	"context"
-	"fmt"
+	"os"
 	"reflect"
 	"sync"
 	"testing"
 	"time"
-
-	"github.com/stianeikeland/go-rpio"
 )
+
+var S Scheduler
+
+func TestMain(m *testing.M) {
+	setup()
+	code := m.Run()
+	shutdown()
+	os.Exit(code)
+}
+
+func setup() {
+	S = Scheduler{
+		wg:            new(sync.WaitGroup),
+		cancellations: make([]context.CancelFunc, 0),
+	}
+}
+
+func shutdown() {
+	S.Stop()
+}
 
 func TestNewScheduler(t *testing.T) {
 	tests := []struct {
@@ -20,10 +36,7 @@ func TestNewScheduler(t *testing.T) {
 	}{
 		{
 			name: "test1",
-			want: &Scheduler{
-				wg:            new(sync.WaitGroup),
-				cancellations: make([]context.CancelFunc, 0),
-			},
+			want: &S,
 		},
 	}
 	for _, tt := range tests {
@@ -49,24 +62,14 @@ func TestScheduler_Add(t *testing.T) {
 	}{
 		{
 			name: "test1",
-			s: &Scheduler{
-				wg:            new(sync.WaitGroup),
-				cancellations: make([]context.CancelFunc, 0),
-			},
+			s:    &S,
 			args: args{
-				ctx: context.WithValue(context.Background(), "values", helpers.ContextWithValue{Name: "test", Pin: rpio.Pin(22)}),
+				ctx: context.Background(),
 				j: func(ctx context.Context) {
-					// Turn off
-					value, ok := ctx.Value("values").(helpers.ContextWithValue)
-					if ok {
-						value.Pin.High()
-						fmt.Printf("Device %s on pin %d turned off at %s\n", value.Name, value.Pin, time.Now().String())
-					} else {
-						fmt.Println("Could not retrieve values from context")
-					}
+					t.Log("TestScheduler_Add")
 				},
 				p: time.Second * 5,
-				o: time.Second * 5,
+				o: 0,
 			},
 		},
 	}
@@ -84,10 +87,7 @@ func TestScheduler_Stop(t *testing.T) {
 	}{
 		{
 			name: "test1",
-			s: &Scheduler{
-				wg:            new(sync.WaitGroup),
-				cancellations: make([]context.CancelFunc, 0),
-			},
+			s:    &S,
 		},
 	}
 	for _, tt := range tests {
@@ -97,45 +97,39 @@ func TestScheduler_Stop(t *testing.T) {
 	}
 }
 
-//func TestScheduler_process(t *testing.T) {
-//	ctxWithCancel, _ := context.WithCancel(context.WithValue(context.Background(), "values", helpers.ContextWithValue{Name: "test", Pin: rpio.Pin(22)}))
-//	type args struct {
-//		ctx context.Context
-//		j   Job
-//		p   time.Duration
-//		o   time.Duration
-//	}
-//	tests := []struct {
-//		name string
-//		s    *Scheduler
-//		args args
-//	}{
-//		{
-//			name: "test1",
-//			s: &Scheduler{
-//				wg:            new(sync.WaitGroup),
-//				cancellations: make([]context.CancelFunc, 0),
-//			},
-//			args: args{
-//				ctx: ctxWithCancel,
-//				j: func(ctx context.Context) {
-//					// Turn off
-//					value, ok := ctx.Value("values").(helpers.ContextWithValue)
-//					if ok {
-//						value.Pin.High()
-//						fmt.Printf("Device %s on pin %d turned off at %s\n", value.Name, value.Pin, time.Now().String())
-//					} else {
-//						fmt.Println("Could not retrieve values from context")
-//					}
-//				},
-//				p: time.Second * 5,
-//				o: time.Second * 5,
-//			},
-//		},
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			tt.s.process(tt.args.ctx, tt.args.j, tt.args.p, tt.args.o)
-//		})
-//	}
-//}
+func TestScheduler_process(t *testing.T) {
+	ctxWithCancel, cancel := context.WithCancel(context.Background())
+	S.cancellations = append(S.cancellations, cancel)
+	S.wg.Add(1)
+
+	type args struct {
+		ctx context.Context
+		j   Job
+		p   time.Duration
+		o   time.Duration
+	}
+	tests := []struct {
+		name string
+		s    *Scheduler
+		args args
+	}{
+		{
+			name: "test1",
+			s:    &S,
+			args: args{
+				ctx: ctxWithCancel,
+				j: func(ctx context.Context) {
+					t.Log("TestScheduler_process")
+					cancel()
+				},
+				p: time.Second * 1,
+				o: 0,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.s.process(tt.args.ctx, tt.args.j, tt.args.p, tt.args.o)
+		})
+	}
+}
