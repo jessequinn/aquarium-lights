ca35beab24d3ddbde228025ddeaa770aa0f8e359
Jesse Quinn <me@jessequinn.info>
2021-06-25 08:09:34 -0300

    Update: unit tests.

--- internal/models/schedules_test.go
+++ internal/models/schedules_test.go
@@ -1,7 +1,9 @@
 package models
 
 import (
+	"reflect"
 	"testing"
+	"time"
 
 	"github.com/stianeikeland/go-rpio"
 )
@@ -55,6 +57,52 @@ func TestSchedules_SetHigh(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			tt.s.SetHigh()
+		})
+	}
+}
+
+func TestSchedules_GetSchedules(t *testing.T) {
+	timeNow := time.Now()
+	tests := []struct {
+		name string
+		s    *Schedules
+		want []Schedule
+	}{
+		{
+			name: "test1",
+			s: &Schedules{
+				Schedules: []Schedule{
+					{
+						Name: "test1",
+						Pin:  rpio.Pin(22),
+						Periods: []Period{
+							{
+								Start: CustomTime{timeNow},
+								End:   CustomTime{timeNow},
+							},
+						},
+					},
+				},
+			},
+			want: []Schedule{
+				{
+					Name: "test1",
+					Pin:  rpio.Pin(22),
+					Periods: []Period{
+						{
+							Start: CustomTime{timeNow},
+							End:   CustomTime{timeNow},
+						},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.s.GetSchedules(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Schedules.GetSchedules() = %v, want %v", got, tt.want)
+			}
 		})
 	}
 }
